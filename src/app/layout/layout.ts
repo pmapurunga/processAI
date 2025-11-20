6bdde9b408699fb315399f6098735239566ad044
paulomapurunga <paulomapurunga@gmail.com>
2025-11-20 10:22:53 -0300

    atualização Layout

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -3,10 +3,26 @@ import { CommonModule } from '@angular/common';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { toSignal } from '@angular/core/rxjs-interop';
+// Angular Material modules
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-layout',
-  imports: [CommonModule, RouterOutlet, RouterLink],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    // Material
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './layout.html',
   styleUrls: ['./layout.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
